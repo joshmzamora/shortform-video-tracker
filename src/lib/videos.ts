d39370a2f7d4cbf00b89f1064a6d7096df71636e
Josh <cooljosh0327@gmail.com>
2026-01-29 16:13:35 +0000

    Add youtube videos when the experiment starts

--- src/lib/videos.ts
+++ src/lib/videos.ts
@@ -2,36 +2,20 @@ export type Video = {
   id: string; // youtube video id
   user: string;
   caption: string;
-  genre: 'Dance' | 'Cooking' | 'Comedy' | 'Animals' | 'DIY' | 'Travel' | 'Music' | 'Art' | 'Tech';
+  genre: 'Educational' | 'Comedy' | 'Doomscrolling' | 'Inspirational' | 'Slice of Life';
   src: string; // youtube embed url
 };
 
+// Shuffled list of videos for the experiment
 export const videos: Video[] = [
-    // Dance
-    { id: 'C-u5WLJ9Yk4', user: '@danceLover', caption: 'Amazing choreography!', genre: 'Dance', src: 'https://www.youtube.com/embed/C-u5WLJ9Yk4' },
-    { id: '2Vv-BfVoq4g', user: '@streetDancer', caption: 'Hip-hop moves', genre: 'Dance', src: 'https://www.youtube.com/embed/2Vv-BfVoq4g' },
-    // Cooking
-    { id: 'bb295_z2iX8', user: '@masterChef', caption: 'Delicious pasta recipe', genre: 'Cooking', src: 'https://www.youtube.com/embed/bb295_z2iX8' },
-    { id: '3G3QIz72-2A', user: '@quickBites', caption: '5-minute breakfast', genre: 'Cooking', src: 'https://www.youtube.com/embed/3G3QIz72-2A' },
-    // Comedy
-    { id: 'SH_S4q6_3x4', user: '@joker', caption: 'Funniest stand-up ever', genre: 'Comedy', src: 'https://www.youtube.com/embed/SH_S4q6_3x4' },
-    { id: 'O1_fkq32SjU', user: '@laughOutLoud', caption: 'Try not to laugh', genre: 'Comedy', src: 'https://www.youtube.com/embed/O1_fkq32SjU' },
-    // Animals
-    { id: 'jPS5dI2gKZA', user: '@animalPlanet', caption: 'Cute puppies', genre: 'Animals', src: 'https://www.youtube.com/embed/jPS5dI2gKZA' },
-    { id: 'E2aFpFyD3sA', user: '@wildLife', caption: 'Funny cat moments', genre: 'Animals', src: 'https://www.youtube.com/embed/E2aFpFyD3sA' },
-    // DIY
-    { id: 'oV24Tf2tO9s', user: '@diyCreator', caption: 'Awesome DIY project', genre: 'DIY', src: 'https://www.youtube.com/embed/oV24Tf2tO9s' },
-    { id: 'v250x642sog', user: '@crafty', caption: 'Home decor ideas', genre: 'DIY', src: 'https://www.youtube.com/embed/v250x642sog' },
-    // Travel
-    { id: 'A92t8iQ_gtI', user: '@traveller', caption: 'Exploring Bali', genre: 'Travel', src: 'https://www.youtube.com/embed/A92t8iQ_gtI' },
-    { id: '1zpf8H_e43I', user: '@wanderer', caption: 'A trip to the Swiss Alps', genre: 'Travel', src: 'https://www.youtube.com/embed/1zpf8H_e43I' },
-    // Music
-    { id: 'ZbZSe6N_BXs', user: '@musicFeels', caption: 'Acoustic cover', genre: 'Music', src: 'https://www.youtube.com/embed/ZbZSe6N_BXs' },
-    { id: 'ktvTqknDobU', user: '@popStar', caption: 'Official music video', genre: 'Music', src: 'https://www.youtube.com/embed/ktvTqknDobU' },
-    // Art
-    { id: 'yX9sQYp4_3s', user: '@artist', caption: 'Speed painting', genre: 'Art', src: 'https://www.youtube.com/embed/yX9sQYp4_3s' },
-    { id: '0fC_olpE0T4', user: '@creativeMind', caption: 'Abstract art tutorial', genre: 'Art', src: 'https://www.youtube.com/embed/0fC_olpE0T4' },
-    // Tech
-    { id: 'iG2de2_2j4o', user: '@techGuru', caption: 'Latest smartphone review', genre: 'Tech', src: 'https://www.youtube.com/embed/iG2de2_2j4o' },
-    { id: 'd_RTySKSz20', user: '@gadgetGeek', caption: 'Top 5 gadgets of 2024', genre: 'Tech', src: 'https://www.youtube.com/embed/d_RTySKSz20' },
+    { id: 'TStUl8bS-lY', user: '@Goalcast', caption: 'The Wisdom of a Third Grade Dropout Will Change Your Life', genre: 'Inspirational', src: 'https://www.youtube.com/embed/TStUl8bS-lY' },
+    { id: '8_Buo2E4s-c', user: '@ScienceFacts', caption: 'Did You Know? Earth\'s Weirdest Facts!', genre: 'Educational', src: 'https://www.youtube.com/embed/8_Buo2E4s-c' },
+    { id: 'SH_S4q6_3x4', user: '@KevinHart', caption: 'That one time in the car...', genre: 'Comedy', src: 'https://www.youtube.com/embed/SH_S4q6_3x4' },
+    { id: 'RHeV4dTM8wE', user: '@CozyVibes', caption: 'Some things that are just *nice*', genre: 'Slice of Life', src: 'https://www.youtube.com/embed/RHeV4dTM8wE' },
+    { id: 'asfRS0CmLXk', user: '@ClimateNews', caption: 'UN chief says climate change is "out of control"', genre: 'Doomscrolling', src: 'https://www.youtube.com/embed/asfRS0CmLXk' },
+    { id: 'w-h_b2i0V3I', user: '@AstroKobi', caption: 'Mind-Blowing Space Facts You Didn\'t Know! 🚀', genre: 'Educational', src: 'https://www.youtube.com/embed/w-h_b2i0V3I' },
+    { id: '2z3wIu6a218', user: '@MoralStories', caption: 'Believe in Yourself | A little story that will change your life', genre: 'Inspirational', src: 'https://www.youtube.com/embed/2z3wIu6a218' },
+    { id: 'O1_fkq32SjU', user: '@FunnyClips', caption: 'When you think you\'re a pro', genre: 'Comedy', src: 'https://www.youtube.com/embed/O1_fkq32SjU' },
+    { id: '_soV9d1S-kM', user: '@TheSchoolOfLife', caption: 'Why The World Is A Lot Better Than You Think', genre: 'Doomscrolling', src: 'https://www.youtube.com/embed/_soV9d1S-kM' },
+    { id: 'I2r2R532g8E', user: '@DayInTheLife', caption: 'a realistic and productive day in my life', genre: 'Slice of Life', src: 'https://www.youtube.com/embed/I2r2R532g8E' },
 ];
